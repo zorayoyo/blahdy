1ec69261f69346294e80b5d59c674b36f34ce528
Shellex Wai <5h3ll3x@gmail.com>
2012-04-26 18:38:38 +0800

    daemon of blahdy.

---
+++ blahdyd/bd.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"github.com/shellex/tattoo/webapp"
+)
+
+var useFCGI = flag.Bool("fcgi", false, "Use FastCGI")
+
+func main() {
+	flag.Parse()
+
+	app := webapp.App{}
+	app.Log("App Starts", "OK")
+	app.SetHandler("/", HandleRoot)
+
+	// Start Server.
+	if *useFCGI {
+		log.Printf("Server Starts(FastCGI): Listen on port %d\n", 4321)
+		app.RunCGI(4321)
+	} else {
+		log.Printf("Server Starts: Listen on port %d\n", 8888)
+		app.Run(8888)
+	}
+}
